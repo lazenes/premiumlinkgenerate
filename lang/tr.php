51fe8ea4924ecec3930134b18758b775284b89ae
lazenes <lazenes@gmail.com>
2016-06-29 14:37:09 +0300

    Create tr.php

---
+++ lang/tr.php
@@ -0,0 +1,15 @@
+<?php
+$lang[1]="Navigasyonu Gizle";
+$lang[2]="Anasayfa";
+$lang[3]="İletişim";
+$lang[4]="Tüm Hakları Saklıdır";
+$lang[5]="Bağiş Yap"; 
+$lang[6]="İndir";
+$lang[7]="Bir Dosya linki girin ve İndir Butonuna basın...";
+$lang[8]="Dosyayı İndirmek İçni Tıklayın";
+$lang[9]="Dosya Boyutu : ";
+$lang[10]="<b style='background-color: red; color:white;'> Dosya Boyutu 150 MB dan fazla olamaz</b>";
+$lang[11]="Durum : ";
+$lang[12]="Dosya İsmi : ";
+
+?>
